9936301a93d8434e79256b54bc639a40148623dc
Abdul Jalil <fuseiniabduljalil180@gmail.com>
2026-05-04 13:40:08 +0400

    changed

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def safe_print_division(a,b):
+def safe_print_division(a , b):
     try:
-        result=a/b
+        result = a / b
     except :
-        result=None
+        result = None
     finally :
-        print("inside result:{}".format(result)
+        print("Inside result:{}".format(result))
     return result
 
